45cc1f7b4dc0aa177cf424591ec5b5a93636d88e
chairman98 <admao8898@gmail.com>
2019-02-16 13:54:32 -0800

    SpaceMovement
    SpaceShip can be moved by WASD

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed; //control the speed of spaceship
+
+    private Rigidbody2D rb2D; 
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D> (); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2D.AddForce(movement * speed);
+    }
+}
