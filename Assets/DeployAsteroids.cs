92c63e35449ff2b059aab23fb8577ecc95c84b6e
Rajan Grewal <rajangrewal123@gmail.com>
2019-02-16 14:07:31 -0800

    Added asteroid spawner
    ^^

---
+++ Assets/DeployAsteroids.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployAsteroids : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+    private int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        StartCoroutine(asteroidWave());
+    }
+    private void spawnEnemy()
+    {
+        if (counter <= 20)
+        {
+            GameObject a = Instantiate(asteroidPrefab) as GameObject;
+            a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.y), Random.Range(-screenBounds.y, screenBounds.y));
+            counter++;
+        }
+        
+    }
+    IEnumerator asteroidWave() {
+        while (true) {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+    }
+}
---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
