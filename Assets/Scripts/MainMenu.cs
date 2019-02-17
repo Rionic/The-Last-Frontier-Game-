d9f061b03f94bc770fd564d2e51554c0d2fb9b4a
Angela <tiangela1@gmail.com>
2019-02-17 11:12:35 -0800

    Changed scene.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,8 +8,7 @@ public class MainMenu : MonoBehaviour
   public void PlayGame (string level)
   {
 
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    SceneManager.LoadScene(level);
+   Application.LoadLevel("Level 1");
   }
 
 public void QuitGame ()
