18fd6281acff319523180d72a209edfd4c372ccd
Angela <tiangela1@gmail.com>
2019-02-16 21:57:31 -0800

    Spawned enemies.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public Transform center;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		transform.up = center.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 distance = center.position - transform.position;
+		distance.Normalize ();
+
+		transform.position += distance * speed;
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject spawnPrefab;
+	public float spawnInterval;
+
+	private float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0) {
+			Spawn ();
+			timer = spawnInterval;
+		}
+	}
+
+	void Spawn() {
+		Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(-40f, 40f), 0, 0);
+		Object.Instantiate (spawnPrefab, randomPosition, Quaternion.identity);
+	}
+}
